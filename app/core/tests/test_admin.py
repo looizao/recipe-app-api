554b690872ba06e0dd93772c8ba1bae0a680141a
Luiz Souza <looizao@looizao.com>
2023-08-01 14:48:16 -0300

    created user api]

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -5,7 +5,7 @@ from django.test import Client
 
 
 class AdminSiteTest(TestCase):
-    def setUP(self):
+    def setUp(self):
         self.client = Client()
         self.admin_user = get_user_model().objects.create_superuser(
             email="admin@example.com", password="testpass123"
@@ -22,3 +22,15 @@ class AdminSiteTest(TestCase):
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_edit_user_page(self):
+        url = reverse("admin:core_user_change", args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        url = reverse("admin:core_user_add")
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
